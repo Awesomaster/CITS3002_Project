0f6859322954632dbaafe334ceef0c1a10927ba5
awesomaster <josh@creativeworks.com.au>
2020-05-15 09:05:32 +0800

    updates to headers included

--- main.c
+++ main.c
@@ -1,11 +1,47 @@
 // Starting code for CITS3002 in C
 
-int main(int argc, char **argv) {
-    char *name = argv[0]; // the spoken name of the station that will be used to refer to the station
-    int port = argv[1]; // port that this station is accessed from e.g. http://localhost:port
-    int address = argv[2]; // address for other stations to use to communicate with this station 
+// Headers
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
 
-    char *adjacentStations = argv; // we will be using all arguments argv[3:argc]
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+#include <arpd/inet.h>
+
+#include <netinet/in.h>
+
+#define MAXDATASIZE 100
+
+
+int main(int argc, char **argv) {
+    char *name = argv[1]; // the spoken name of the station that will be used to refer to the station
+    char *tcpPort = argv[2]; // port for tcp connection from e.g. http://localhost:port
+    //int udpPort = argv[3]; // port for udp for other stations to use to communicate with this station 
+
+    //char **adjacentStations = argv; // we will be using all arguments argv[4:argc]
+
+
+
+    
+    struct addrinfo hints, *res;
+    int sockfd;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    getaddrinfo(NULL, tcpPort, &hints, &res);
+
+    sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+
+    bind(sockfd, res->a-_addr, res->ai_addrlen);
+    
 
     /* pseudo code
     if (station in adjacentStations) {
