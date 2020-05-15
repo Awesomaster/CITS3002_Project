fce0f5cbf356afeb7c492831c7d73d14ebac08f3
awesomaster <josh@creativeworks.com.au>
2020-05-15 22:37:43 +0800

    NAME GETTING WORKS!!!!

--- main.c
+++ main.c
@@ -15,18 +15,49 @@
 #include <netinet/in.h>
 
 #define MAXDATASIZE 4096 // Max data size
+#define MAXADJACENT 20 // Max amount of adjacent stations
 #define LISTENQ 4 // Max number of client connections
 
-int tcpConnect(int tcpPort) {
-    return 0;
-}
+char* udpSend(int stationPort, char *message) {
+    struct sockaddr_in udpOutAddress;
+    int udpOutfd;
+    char buf[MAXDATASIZE];
 
-int udpConnect(char *stationName) {
-    return 0;
-}
+    if ((udpOutfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("Failed UDP socket");
+        exit(EXIT_FAILURE);
+    }
 
-int broadcast(char **adjacentStations, char **adjacentPorts) {
+    memset(&udpOutAddress, 0, sizeof(udpOutAddress));
+    udpOutAddress.sin_family = AF_INET;
+    udpOutAddress.sin_port = htons(stationPort);
+    udpOutAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    // Send request to the stationPort
+    printf("Sending %s request to: %i\n", message, stationPort);
+    fflush(stdout);
+    sendto(udpOutfd, message, strlen(message), 0, (struct sockaddr*) &udpOutAddress, sizeof(udpOutAddress));
+
+    /*
+    printf("Waiting for response from port %i", stationPort);
     
+    // Reply from UDP server (not sure if this will get fed through to the other udpfd section, if so we will need to do some more fanangling)
+    bzero(buf, sizeof(buf));
+    recvfrom(udpOutfd, buf, sizeof(buf), 0, (struct sockaddr*) &udpOutAddress, sizeof(udpOutAddress));
+    printf("Reply from UDP Request: ");
+    puts(buf);
+    */
+    close(udpOutfd);
+
+    return buf;
+}
+
+int broadcast(int *adjacentPorts, char *message) {
+    for (int i=0; i<sizeof(adjacentPorts); i++) { // NOTE: Might need to check the sizeof adjacentStations (might need to div by sizeof(int))
+        if(adjacentPorts[i] != 0) {
+            printf(udpSend(adjacentPorts[i], message));
+        }
+    }
     return 0;
 }
 
@@ -36,21 +67,32 @@ int max(int x, int y) {
 }
 
 int main(int argc, char **argv) {
-    
+    printf("Code is starting\n");
+    // Variables defined from argv
     char *name = argv[1]; // the spoken name of the station that will be used to refer to the station
     int tcpPort = atoi(argv[2]); // port for tcp connection from e.g. http://localhost:port
     int udpPort = atoi(argv[3]); // port for udp for other stations to use to communicate with this station 
-    int friendsPort = atoi(argv[4]); // temp
-    //char **adjacentStations = argv; // we will be using all arguments argv[4:argc]
+    
+    int adjacentPorts[MAXADJACENT];
+    int position = 0;
+    bzero(adjacentPorts, sizeof(adjacentPorts));
 
-    tcpConnect(tcpPort);
+    while (argv[position+4][0] != '?') {
+        adjacentPorts[position] = atoi(argv[position+4]);
+        position++;
+    } 
 
+    // Variabled required for socket connections
     int listenfd, udpfd, maxfd, connfd, externalfd, n, nready;
     fd_set rset;
     pid_t childpid;
     socklen_t clientLen;
     char buf[MAXDATASIZE];
     struct sockaddr_in clientAddress, tcpServerAddress, udpServerAddress, externalUdpServerAddress;
+
+    // Regex variables
+    regex_t regex;
+    int regexCheck = 0;
 
     // TCP Setup
     listenfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -74,17 +116,23 @@ int main(int argc, char **argv) {
 
     printf("%s\n", "Servers running... waiting for connections...");    
 
-    FD_ZERO(&rset);
+    // This is to find the name of all adjacent stations
+    char broadcastMessage[50];
+    strcpy(broadcastMessage, "NAME:");
+    strcat(broadcastMessage, argv[3]);
+    broadcast(adjacentPorts, broadcastMessage);
 
+    FD_ZERO(&rset);
     maxfd = max(listenfd, udpfd) + 1;
 
     // Listen to UDP and TCP
+    printf("Listening on ports\n");
     for(;;) {
         FD_SET(listenfd, &rset);
         FD_SET(udpfd, &rset);
 
         nready = select(maxfd, &rset, NULL, NULL, NULL);
-
+        
         if (FD_ISSET(listenfd, &rset)) {
             clientLen = sizeof(clientAddress);
             connfd = accept(listenfd, (struct sockaddr *) &clientAddress, &clientAddress);
@@ -98,33 +146,20 @@ int main(int argc, char **argv) {
                 printf("%s\n", "String recieved from TCP");
                 strtok(buf, "="); // Get rid of everything before the =
                 char *destinationStation = strtok(0, " "); // Get rid of everything after the destination name
-                
+                char *time = "currentTime";
+
+                char *finalResult = ""; // This will be the time of arrival and path to destination
+
                 // Here we become a UDP client after we find the port of the station we are trying to get to
-
-                // First step is to see if its within our adjacent stations (meaning that we need to know the names of those stations as well as the ports)
-                // If its not an adjacent port, then we need to find the station, we can do this using some sort of broadcast
+                char request[100];
+                strcpy(request, "PATH:");
+                strcat(request, destinationStation);
+                strcat(request, time); // This will be the message that we send to the next station
                 
-                int nextPort = friendsPort; // The port that we found based on the next station in the path towards the destination station
-                char *request = "Can you please tell me train time"; // This will be the message that we send to the next station
-                externalfd = socket(AF_INET, SOCK_DGRAM, 0); // Need to do error checking on this
-
-                memset(&externalUdpServerAddress, 0, sizeof(externalUdpServerAddress));
-                externalUdpServerAddress.sin_family = AF_INET;
-                externalUdpServerAddress.sin_port = htons(nextPort);
-                externalUdpServerAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-                // Send request to the next server
-                sendto(externalfd, request, strlen(request), 0, (struct sockaddr*) &externalUdpServerAddress, sizeof(externalUdpServerAddress));
-
-                // Reply from UDP server (not sure if this will get fed through to the other udpfd section, if so we will need to do some more fanangling)
-                bzero(buf, sizeof(buf));
-                recvfrom(externalfd, buf, sizeof(buf), 0, (struct sockaddr*) &externalUdpServerAddress, sizeof(externalUdpServerAddress));
-                printf("Reply from UDP Request: ");
-                puts(buf);
-                close(externalfd);
+                broadcast(adjacentPorts, request); // We ask around for if anyone knows how to get to destination
 
                 // Reply to TCP connection
-                send(connfd, destinationStation, strlen(destinationStation), 0);
+                send(connfd, finalResult, strlen(finalResult), 0); // Send time of arrival and path of destination back to the web
                 printf("Replied to TCP Connection. TCP Over.\n");
                 
                 close(connfd);
@@ -135,27 +170,46 @@ int main(int argc, char **argv) {
 
         // UDP Server
         if (FD_ISSET(udpfd, &rset)) {
-            clientLen = sizeof(clientAddress);
-            printf("%s\n", "String recieved from UDP");
+            printf("%s", "String recieved from UDP: ");
             bzero(buf, sizeof(buf));
-            n = recvfrom(udpfd, buf, MAXDATASIZE, 0, (struct sockaddr*) &clientAddress, &clientLen);
+            recvfrom(udpfd, buf, MAXDATASIZE, 0, (struct sockaddr*) &clientAddress, sizeof(clientAddress));
             puts(buf);
-            char *reply = "thank you for your message, from udp server";
-            sendto(udpfd, reply, strlen(reply), 0, (struct sockaddr*) &clientAddress, sizeof(clientAddress)); 
+
+            // Check the request (using regex)
+            regexCheck = regcomp(&regex, "NAME", 0);
+            regexCheck = regexec(&regex, buf, 0, NULL, 0); // This will check if "NAME" as a string exists within the UDP message, meaning that they want to know our name 
+            char sendingName[50];
+            strcpy(sendingName, "IAM:");
+            strcat(sendingName, name);
+            strcat(sendingName, ":");
+            strcat(sendingName, argv[2]);
+            if (regexCheck == 0) { // They are asking for our name
+                printf("Regex Check Success: IAM\n");
+                strtok(buf,":");
+                int returnPort = atoi(strtok(0,":"));
+                udpSend(returnPort, sendingName);
+                //sendto(connfd, sendingName, strlen(sendingName), 0, (struct sockaddr*) &clientAddress, sizeof(clientAddress)); 
+                printf("Replied to NAME request with: %s\n", sendingName);
+            } 
+            
+            regexCheck = regcomp(&regex, "PATH:", 0);
+            regexCheck = regexec(&regex, buf, 0, NULL, 0); // This will check if "PATH" as a string exists within the UDP message, meaning that they want to know how to get somewhere
+            if (regexCheck == 0) { // They are asking for the path to their destination
+                printf("Regex Check Success: PATH\n");
+                char *reply = "idk yet";
+                sendto(connfd, reply, strlen(reply), 0, (struct sockaddr*) &clientAddress, sizeof(clientAddress)); 
+            
+            } 
+
+            regexCheck = regcomp(&regex, "IAM:", 0);
+            regexCheck = regexec(&regex, buf, 0, NULL, 0); // This will be a reply
+            if (regexCheck == 0) { // This is a reply meaning that they have given us their name
+                printf("Regex Check Success: NAME\n");
+                printf("This was a name\n");
+                // First you have to run this: strtok(buf, ":");
+                // This is their name: strtok(0, ":");
+                // This is their port: strtok(0, ":");
+            }
         }
     }
-
-
-    /* pseudo code
-    if (station in adjacentStations) {
-        return nextTrip
-    } else {
-        findStation (by looking at adjacent stations)
-        addNewStationsFound (to address book)
-        findRoute to station
-        return stepsOfRoute
-    }
-
-    */
-}
-
+}   
